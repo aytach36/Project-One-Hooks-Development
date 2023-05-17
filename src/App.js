b61e18cf8524a3443d3527f89b07b53ce005030b
AytachS <aytchkf@gmail.com>
2023-05-17 11:17:13 +0300

    8. useEffect

--- src/App.js
+++ src/App.js
@@ -1,33 +1,38 @@
 import { useState } from "react";
+import Joke from "./Joke";
 
 function App() {
-  const [userQuery, setUserQuery] = useState('');
+  const [userQuery, setUserQuery] = useState("");
 
   const updateUserQuery = (e) => {
-    console.log('userQuery', userQuery);
-    setUserQuery(e.target.value)
-  }
+    console.log("userQuery", userQuery);
+    setUserQuery(e.target.value);
+  };
 
   const searchQuery = () => {
-    window.open(`https://google.com/search?q=${userQuery}`, '_blank');
-  }
+    window.open(`https://google.com/search?q=${userQuery}`, "_blank");
+  };
 
-  const handleKeyPress = e => {
-    if (e.key === 'Enter') {
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter") {
       console.log(e.key);
       searchQuery();
     }
-  }
-
-
+  };
 
   return (
     <div className="App">
       <h1>Hello Aytach</h1>
       <div className="form">
-        <input onKeyDown={handleKeyPress} value={userQuery} onChange={updateUserQuery} />
-        <button  onClick={searchQuery}>Search</button>
+        <input
+          onKeyDown={handleKeyPress}
+          value={userQuery}
+          onChange={updateUserQuery}
+        />
+        <button onClick={searchQuery}>Search</button>
       </div>
+      <hr />
+      <Joke />
     </div>
   );
 }
