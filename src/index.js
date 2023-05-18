e5376f32b49a0364ec24339f171cfbb35d3e7bff
AytachS <aytchkf@gmail.com>
2023-05-18 10:52:39 +0300

    11. [Optional] Function Keyword vs Arrow Functions (see index.js commented part)

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <App />
-);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// function createObject() {
+//   console.log("outermost this", this);
+
+//   return {
+//     arrowFunction: () => {
+//       console.log("arrowFunction this", this);
+//     },
+//     functionKeywordFunction: function () {
+//       console.log("functionKeyword this", this);
+//     },
+//   };
+// }
+
+// const obj = createObject();
+// console.log("obj", obj);
+// obj.arrowFunction();
+// obj.functionKeywordFunction();
+
+root.render(<App />);
