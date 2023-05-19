1cc43f0a6303970ccd5b3a5a202589498744d8d3
AytachS <aytchkf@gmail.com>
2023-05-19 10:22:46 +0300

    17. Tasks Component Part 2

--- src/Tasks.js
+++ src/Tasks.js
@@ -1,37 +1,63 @@
 import { useState } from "react";
-import uuid from 'uuid/v4';
+import uuid from "uuid/v4";
 
 export default function Tasks() {
-    const [taskText, setTaskText] = useState('');
-    const [tasks, setTasks] = useState([]);
-    const [completedTasks, setCompletedTasks] = useState([]);
+  const [taskText, setTaskText] = useState("");
+  const [tasks, setTasks] = useState([]);
+  const [completedTasks, setCompletedTasks] = useState([]);
 
-    const updateTaskText = event => {
-        setTaskText(event.target.value);
-    }
+  const updateTaskText = (event) => {
+    setTaskText(event.target.value);
+  };
 
-    const addTask = () => {
-        setTasks([...tasks, { taskText, id: uuid() }]);
-    };
+  const addTask = () => {
+    setTasks([...tasks, { taskText, id: uuid() }]);
+  };
 
-    console.log('tasks', tasks);
+  const completeTask = (completedTask) => () => {
+    setCompletedTasks([...completedTasks, completedTask]);
+    setTasks((prevTasks) =>
+      prevTasks.filter((task) => task.id !== completedTask.id)
+    );
+  };
 
-    return (
-        <div>
-            <h3>Tasks</h3>
-            <div className="form">
-                <input value={taskText} onChange={updateTaskText} />
-                <button onClick={addTask}>Add Task</button>
+  const deleteTask = (task) => () => {
+    setCompletedTasks((prev) => prev.filter((t) => t.id !== task.id));
+  };
+
+  console.log("tasks", tasks);
+  console.log("completed tasks", completedTasks);
+
+  return (
+    <div>
+      <h3>Tasks</h3>
+      <div className="form">
+        <input value={taskText} onChange={updateTaskText} />
+        <button onClick={addTask}>Add Task</button>
+      </div>
+      <div className="task-list">
+        {tasks.map((task) => {
+          const { id, taskText } = task;
+          return (
+            <div key={id} onClick={completeTask(task)}>
+              {taskText}
             </div>
-            <div className="task-list">
-                {
-                    tasks.map(task => {
-                        const { id, taskText } = task;
-                        return <div key={id}>{taskText}</div>
-                    })
-                }
+          );
+        })}
+      </div>
+      <div className="completed-list">
+        {completedTasks.map((task) => {
+          const { id, taskText } = task;
+          return (
+            <div key={id}>
+              {taskText}{" "}
+              <span onClick={deleteTask(task)} className="delete-task">
+                x
+              </span>
             </div>
-        </div>
-    )
-
+          );
+        })}
+      </div>
+    </div>
+  );
 }
