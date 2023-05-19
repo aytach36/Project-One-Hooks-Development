fe0179957106b3fd0f253f3db01b6e04a3a5be1c
aytach36 <aytchkf@gmail.com>
2023-05-19 22:06:05 +0300

    21. Hooks with an Interval

---
+++ src/Gallery.js
@@ -0,0 +1,23 @@
+import { useState, useEffect } from "react";
+import PICTURES from './data/pictures';
+
+export default function Gallery() {
+    const [index, setIndex] = useState(0);
+
+    useEffect(() => {
+        setInterval(() => {
+            setIndex(
+                storedIndex => {
+                    return (storedIndex + 1) % PICTURES.length;
+                }
+            )
+        }, 3000);
+    }, []);
+
+    return (
+        <div className="Gallery">
+            <img src={PICTURES[index].image} alt='gallery' />
+        </div>
+    )
+};
+
