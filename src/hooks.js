9ae61bd7080d27f11f3e453dcb2e524c718f1fe0
aytach36 <aytchkf@gmail.com>
2023-05-23 22:20:56 +0300

    Matrix Component & useDynamicTransition custom Hook | SECTION 4 COMPLETE

--- src/hooks.js
+++ src/hooks.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 
-const useFetch = (url, initialValue) => {
+export const useFetch = (url, initialValue) => {
     const [result, setResult] = useState(initialValue);
     useEffect(() => {
         fetch(url)
@@ -11,4 +11,22 @@ const useFetch = (url, initialValue) => {
     return result;
 };
 
-export { useFetch };
+export const useDynamicTransition = ({ increment, delay, length }) => {
+    const [index, setIndex] = useState(0);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setIndex(
+                storedIndex => {
+                    return (storedIndex + increment) % length;
+                }
+            )
+        }, delay);
+
+        return () => clearInterval(interval);
+
+    }, [delay, increment]);
+
+    return index;
+
+}
